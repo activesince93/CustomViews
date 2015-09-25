5a2b6531b6ffb02b01cd7dba9ae74926dbd25cc9
Darshan Parikh <parikhdarshan36@gmail.com>
2015-09-26 00:52:11 +0530

    Custom TextViews added

---
+++ app/src/main/java/active/since93/customviewsdemo/TextViewsActivity.java
@@ -0,0 +1,16 @@
+package active.since93.customviewsdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Darshan on 26-Sep-15.
+ */
+public class TextViewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_textviews);
+    }
+}
