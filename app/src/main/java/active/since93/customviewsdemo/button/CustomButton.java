91f422765d47e29baeaea797527f5af319002784
Darshan Parikh <parikhdarshan36@gmail.com>
2015-09-28 15:14:18 +0530

    Custom button

---
+++ app/src/main/java/active/since93/customviewsdemo/button/CustomButton.java
@@ -0,0 +1,41 @@
+package active.since93.customviewsdemo.button;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+import active.since93.customviewsdemo.R;
+
+
+/**
+ * Created by darshan.parikh on 28-Sep-15.
+ */
+public class CustomButton extends Button {
+    public CustomButton(Context context) {
+        super(context);
+        init(null);
+    }
+
+    public CustomButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+    }
+
+    public CustomButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs);
+    }
+
+    private void init(AttributeSet attrs) {
+        TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.CustomButton);
+        String text = a.getString(R.styleable.CustomButton_text);
+        int color = a.getColor(R.styleable.CustomButton_text_color, Color.BLACK);
+        float size = a.getDimension(R.styleable.CustomButton_text_size, 15);
+        setText(text);
+        setTextSize(size);
+        setTextColor(color);
+        a.recycle();
+    }
+}
