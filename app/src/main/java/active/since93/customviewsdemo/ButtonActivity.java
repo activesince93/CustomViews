6bb7ae1143aa7858aeb62ed24ccb541b7afeb2fc
Darshan Parikh <parikhdarshan36@gmail.com>
2015-09-28 15:15:08 +0530

    Custom button activity

---
+++ app/src/main/java/active/since93/customviewsdemo/ButtonActivity.java
@@ -0,0 +1,15 @@
+package active.since93.customviewsdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by darshan.parikh on 28-Sep-15.
+ */
+public class ButtonActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buttons);
+    }
+}
