fab6eb56ea088f44ba9c12eabac77d510d0b7467
Darshan Parikh <parikhdarshan36@gmail.com>
2015-09-28 15:29:46 +0530

    minor change

--- app/src/main/java/active/since93/customviewsdemo/ButtonActivity.java
+++ app/src/main/java/active/since93/customviewsdemo/ButtonActivity.java
@@ -1,12 +1,12 @@
 package active.since93.customviewsdemo;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by darshan.parikh on 28-Sep-15.
  */
-public class ButtonActivity extends Activity {
+public class ButtonActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
