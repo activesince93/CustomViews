169ef421abe36f2c5eade84e9b69c5a9a28a36d5
Darshan Parikh <parikhdarshan36@gmail.com>
2015-10-07 16:01:07 +0530

    floating edittext

--- app/src/main/java/active/since93/customviewsdemo/EditTextsActivity.java
+++ app/src/main/java/active/since93/customviewsdemo/EditTextsActivity.java
@@ -1,17 +1,30 @@
 package active.since93.customviewsdemo;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 /**
  * Created by darshan.parikh on 07-Oct-15.
  */
 public class EditTextsActivity extends AppCompatActivity {
+
+    TextInputLayout textInputLayout;
+    EditText editText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edittexts);
+
+        textInputLayout = (TextInputLayout) findViewById(R.id.layoutEdtTxt);
+        editText = (EditText) findViewById(R.id.extTxtEnter);
+
+        textInputLayout.setErrorEnabled(true);
+        textInputLayout.setError("Min 2 chars required");
+        editText.setError("Required");
     }
 
     @Override
